a5d823ccafc7ac1eb181baf06d49958af3e9105d
chenlyuping <chenlyuping@gmail.com>
2017-04-18 22:05:41 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -44,14 +44,10 @@ end
   redirect_to jobs_path
 end
 
-def job_params
-  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
-end
-
 private
 
 def job_params
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
 end
 
 end
