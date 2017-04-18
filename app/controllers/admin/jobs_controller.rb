5fa39c38801b1c09034501dc46b2fcd7cf61e8ec
chenlyuping <chenlyuping@gmail.com>
2017-04-18 21:59:17 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -44,11 +44,8 @@ def destroy
   redirect_to admin_jobs_path
 end
 
-def require_is_admin
-  if !current_user.admin?
-    flash[:alert] = 'You are not admin'
-    redirect_to root_path
-  end
+def job_params
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
 end
 
 private
